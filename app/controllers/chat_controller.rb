4e3c68053193768a99dfc9c1872e5c377d2579b1
kuriyama <ipod0909goo@gmail.com>
2014-07-11 21:42:16 +0900

    create chat using Websocket

---
+++ app/controllers/chat_controller.rb
@@ -0,0 +1,7 @@
+class ChatController < ApplicationController
+  # 特に利用していない
+  
+   # ただ画面を表示しているだけ
+  def index
+  end
+end
---
+++ app/controllers/websocket_chat_controller.rb
@@ -0,0 +1,11 @@
+# WebsocketRails::BaseControllerを継承
+
+class WebsocketChatController < WebsocketRails::BaseController
+  def receive_message
+    # クライアントからのメッセージを取得
+    receive_message = message()
+  
+    # websocket_chatイベントで接続しているクライアントにブロードキャスト
+    broadcast_message(:websocket_chat, receive_message);
+  end
+end
